313779d0c83cc8f1eca7d7737ee187ee224358c7
San7122 <sanjanathakur302@gmail.com>
2026-01-17 15:17:08 +0530

    Fix: Add missing authorize function to auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -60,3 +60,24 @@ exports.checkPlan = (...plans) => {
     next();
   };
 };
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        error: 'User not authenticated'
+      });
+    }
+    
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        error: `User role '${req.user.role}' is not authorized to access this route`
+      });
+    }
+    
+    next();
+  };
+};
